818e0037be035b4204bc312e30b8ccef60f62893
Marek Seablom-Michel <marek.seablommichel@gmail.com>
2026-05-04 00:08:28 -0700

    feat: task 1 complete

--- proj2.py
+++ proj2.py
@@ -1,11 +1,27 @@
+from __future__ import annotations
+import sys
 import csv
-import math
-from dataclasses import dataclass
 from typing import *
+from dataclasses import dataclass
+import math
 
 
 # Put your data definitions first!
+@dataclass(frozen=True)
+class Row:
+    country: str
+    year: int
+    electricity_and_heat_co2_emissions: float | None
+    electricity_and_heat_co2_emissions_per_capita: float | None
+    energy_co2_emissions: float | None
+    energy_co2_emissions_per_capita: float | None
+    total_co2_emissions_excluding_lucf: float | None
+    total_co2_emissions_excluding_lucf_per_capita: float | None
 
+@dataclass(frozen=True)
+class Node:
+    value: Row
+    next: Node | None
 # ...
 
 # Then your functions.
